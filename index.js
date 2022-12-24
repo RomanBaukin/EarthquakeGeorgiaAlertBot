ed22009206f8b8fd7826322a9dbbbd62b2f028e9
Roman Baukin <rbaukin1984@gmail.com>
2022-12-24 17:19:59 +0400

    feat: add generationMessage function & 10_recent_earthquakes command

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const jsdom = require("jsdom");
 const {
   JSDOM
 } = jsdom;
-
-let str = '';
+const earthquakes = [];
 
 axios.get(link, {
     headers: {
@@ -22,10 +21,19 @@ axios.get(link, {
     const dom = new JSDOM(response.data);
     const table = dom.window.document.querySelector('.eartquakes-table tbody');
 
-    for (let i = 0; i < 5; i++) {
-      str += `${table.childNodes[i].childNodes[0].textContent} | магнитуда ${table.childNodes[i].childNodes[1].textContent} | глубина ${table.childNodes[i].childNodes[3].textContent} км | координаты ${table.childNodes[i].childNodes[4].textContent} | регион ${table.childNodes[i].childNodes[5].textContent}\n\n`
+    for (let i = 0; i < 10; i++) {
+      const tempObj = {
+        time: table.childNodes[i].childNodes[0].textContent,
+        magnitude: table.childNodes[i].childNodes[1].textContent,
+        depth: table.childNodes[i].childNodes[3].textContent,
+        coordinates: table.childNodes[i].childNodes[4].textContent,
+        region: table.childNodes[i].childNodes[5].textContent
+      };
+
+      earthquakes.push(tempObj);
     }
-    console.log(str);
+
+    console.log(earthquakes);
   })
   .catch(function(error) {
     // handle error
@@ -35,7 +43,8 @@ axios.get(link, {
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply(`Привет, ${ctx.message.from.first_name ? ctx.message.from.first_name : 'незнакомец'}!`));
 bot.command('behavior_during_earthquakes', (ctx) => ctx.reply(text.behaviorDuringEarthquakes));
-bot.command('5_recent_earthquakes', (ctx) => ctx.reply(str));
+bot.command('5_recent_earthquakes', (ctx) => ctx.reply(generationMessage(5)));
+bot.command('10_recent_earthquakes', (ctx) => ctx.reply(generationMessage(10)));
 bot.help((ctx) => ctx.reply(text.commands));
 
 bot.launch();
@@ -43,3 +52,13 @@ bot.launch();
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+function generationMessage(amountEarthquake) {
+  let tempStr = `${amountEarthquake} последних землетрясений\n\n`;
+
+  for (let i = 0; i < amountEarthquake; i++) {
+    tempStr += `${earthquakes[i].time} | магнитуда ${earthquakes[i].magnitude} | глубина ${earthquakes[i].depth} | координаты ${earthquakes[i].coordinates} | регион ${earthquakes[i].region}\n\n`;
+  }
+
+  return tempStr;
+}
