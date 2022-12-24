7cb8b9bf00489fd626e956ec67b4aab82b0ef0a3
Roman Baukin <rbaukin1984@gmail.com>
2022-12-24 11:31:26 +0400

    refactor: create the project structure

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const {
   Telegraf
 } = require('telegraf');
 require('dotenv').config();
+const text = require('./const');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) => ctx.reply('Welcome'));
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on('sticker', (ctx) => ctx.reply('👍'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.start((ctx) => ctx.reply(`Привет, ${ctx.message.from.first_name ? ctx.message.from.first_name : 'незнакомец'}!`));
+bot.help((ctx) => ctx.reply(text.commands));
+
 bot.launch();
 
 // Enable graceful stop
