bb97b0173bd8bb5c86cff42246f437b1e67ee65e
Roman Baukin <rbaukin1984@gmail.com>
2023-01-11 11:31:12 +0400

    feat: create generationListEarthquakes function

--- index.js
+++ index.js
@@ -12,36 +12,8 @@ const {
   JSDOM
 } = jsdom;
 const earthquakes = [];
-
-axios.get(link, {
-    headers: {
-      "Accept-Encoding": "gzip,deflate,compress"
-    }
-  })
-  .then(function(response) {
-    // handle success
-    const dom = new JSDOM(response.data);
-    const table = dom.window.document.querySelector('.eartquakes-table tbody');
-
-    for (let i = 0; i < 10; i++) {
-      const tempObj = {
-        time: table.childNodes[i].childNodes[0].textContent,
-        magnitude: table.childNodes[i].childNodes[1].textContent,
-        depth: table.childNodes[i].childNodes[3].textContent,
-        coordinates: table.childNodes[i].childNodes[4].textContent,
-        region: table.childNodes[i].childNodes[5].textContent
-      };
-
-      earthquakes.push(tempObj);
-    }
-  })
-  .catch(function(error) {
-    // handle error
-    console.log(error);
-    bot.telegram.sendMessage(chatIDLog, console.log(error));
-  })
-
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
 bot.start((ctx) => ctx.reply(`Привет, ${ctx.message.from.first_name ? ctx.message.from.first_name : 'незнакомец'}!`));
 bot.command('behavior_during_earthquakes', async (ctx) => await ctx.reply(text.behaviorDuringEarthquakes));
 bot.command('5_recent_earthquakes', async (ctx) => await ctx.reply(generationMessage(5)));
@@ -53,6 +25,36 @@ bot.launch();
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+async function generationListEarthquakes() {
+  axios.get(link, {
+      headers: {
+        "Accept-Encoding": "gzip,deflate,compress"
+      }
+    })
+    .then(function(response) {
+      // handle success
+      const dom = new JSDOM(response.data);
+      const table = dom.window.document.querySelector('.eartquakes-table tbody');
+
+      for (let i = 0; i < 10; i++) {
+        const tempObj = {
+          time: table.childNodes[i].childNodes[0].textContent,
+          magnitude: table.childNodes[i].childNodes[1].textContent,
+          depth: table.childNodes[i].childNodes[3].textContent,
+          coordinates: table.childNodes[i].childNodes[4].textContent,
+          region: table.childNodes[i].childNodes[5].textContent
+        };
+
+        earthquakes.push(tempObj);
+      }
+    })
+    .catch(function(error) {
+      // handle error
+      console.log(error);
+      bot.telegram.sendMessage(chatIDLog, console.log(error));
+    })
+}
 
 function generationMessage(amountEarthquake) {
   let tempStr = `${amountEarthquake} последних землетрясений\n\n`;
@@ -96,4 +98,6 @@ function checkLastEarthquake() {
       bot.telegram.sendMessage(chatIDLog, console.log(error));
     })
 }
+
+generationListEarthquakes();
 setInterval(checkLastEarthquake, 60000);
